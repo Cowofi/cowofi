5027b4057c55437dcad65fa2aec5980797ce652c
Albert Hidalgo <alhidalgodev@gmail.com>
2022-08-13 12:59:56 -0400

    feat
    * Added env vars
    * Created example env

--- quasar.config.js
+++ quasar.config.js
@@ -55,6 +55,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
+      env: require('dotenv').config().parsed,
       target: {
         browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
         node: 'node16'
