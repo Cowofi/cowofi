b388c65a60305089b95396cc75118468cbf6201b
Albert Hidalgo <alhidalgodev@gmail.com>
2022-08-13 22:07:02 -0400

    feat
    * Added explorer route

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'explorer', component: () => import('src/pages/ExplorerArticles.vue') }
     ]
   },
   {
