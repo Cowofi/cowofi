95595164b670166f3318a98f338affea9d29fc76
Albert Hidalgo <alhidalgodev@gmail.com>
2022-08-14 12:19:29 -0400

    feat
    * Use the public dir for assets

--- src/pages/Auth/Register.vue
+++ src/pages/Auth/Register.vue
@@ -18,7 +18,7 @@
       <q-card flat style="max-width: 400px" class="q-mx-auto q-mt-lg">
         <q-card-section class="text-center">
           <q-img
-            src="~assets/main-logo.png"
+            src="images/main-logo.png"
             spinner-color="white"
             style="height: 100px; max-width: 100px"
           />
