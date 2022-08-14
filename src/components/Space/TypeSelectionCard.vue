5ec802db4534015982e3deb79a2635cddc9646e1
Albert Hidalgo <alhidalgodev@gmail.com>
2022-08-14 12:19:08 -0400

    feat
    * Created component to selec the type of space in spaces form

---
+++ src/components/Space/TypeSelectionCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-card class="type-card-container cursor-pointer">
+    <q-card-section>
+      <div class="row justify-center items-center">
+        <div class="col-4">
+          <slot name="img"></slot>
+        </div>
+        <div class="col-8">
+          <span class="text-bold">{{ title }}</span>
+        </div>
+        <div class="col-12">
+          <slot name="selection"></slot>
+        </div>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: "ComponentSpaceType",
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    to: {
+      type: String,
+      default: "#",
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+<style>
+.type-card-container {
+  max-width: 250px;
+  height: 110px;
+}
+</style>
