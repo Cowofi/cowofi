750ca3d5065e47d8161933b531b41bed3cfa31da
Albert Hidalgo <alhidalgodev@gmail.com>
2022-08-13 21:24:06 -0400

    feat
    * Added some home parts

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,41 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page padding>
+    <div class="row justify-center items-center">
+      <div class="col-4">
+        <p class="text-h3 text-bold">
+          {{ $t("messages.information.findPerfectWorkingSpaceNearYou") }}!
+        </p>
+      </div>
+      <div class="col-8 text-right">
+        <img
+          alt="work time illustration"
+          src="~assets/illustrations/work_time.png"
+          style="width: 600px"
+        />
+      </div>
+    </div>
+    <div class="bg-secondary q-pa-md">
+      <div class="row">
+        <div class="col-12">
+          <article-type :title="$t('common.chillOutZone')">
+            <template v-slot:img>
+              <img width="50" src="~assets/illustrations/chill_zone.png" />
+            </template>
+          </article-type>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
+import ArticleType from "components/Article/TypeCard.vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+  components: {
+    ArticleType,
+  },
+});
 </script>
