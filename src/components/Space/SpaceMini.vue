019bbbe8a50c134299ef6e0c79e2dfc20387eb3f
Albert Hidalgo <alhidalgodev@gmail.com>
2022-08-15 21:22:49 -0400

    feat
    * UI/UX

--- src/components/Space/SpaceMini.vue
+++ src/components/Space/SpaceMini.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-card flat bordered>
+  <q-card class="cursor-pointer" flat bordered style="max-width: 650px">
     <q-card-section>
       <div class="row q-col-gutter-md space-mini-container">
         <div class="col-4">
@@ -12,20 +12,50 @@
         <div class="col-8">
           <div class="row">
             <div class="col-12">
-              <p class="elipsis-2">
+              <p class="ellipsis-2-lines">
                 {{ space.description }}
               </p>
             </div>
             <div class="col-12">
-              <space-type-selection :title="spaceType.title">
-                <template v-slot:img>
+              <div class="row items-center">
+                <div class="col-2">
                   <img width="50" :src="spaceType.img" />
-                </template>
-              </space-type-selection>
+                </div>
+                <div class="col-10">
+                  {{ spaceType.title }}
+                  <template v-if="space.private_office">
+                    <q-chip
+                      color="primary"
+                      text-color="white"
+                      icon="eva-lock-outline"
+                    >
+                      {{ $t("common.privateOffice") }}
+                    </q-chip>
+                  </template>
+                </div>
+              </div>
 
-              {{ space.internet }} {{ space.internet_speed }}
+              <div class="text-grey text-italic">
+                {{ space.country }} - {{ space.city }}
+              </div>
+
+              <div class="q-mt-sm">
+                <q-icon size="sm" name="eva-clock-outline" />
+
+                {{ $t("common.opensAt") }} {{ parseTime(space.opens_at) }} -
+                {{ $t("common.closesAt") }} {{ parseTime(space.closes_at) }}
+              </div>
+
+              <q-separator class="q-my-md" />
+              <template v-if="space.internet">
+                <q-icon size="sm" name="eva-wifi-outline" />
+                {{ space.internet_speed }}
+              </template>
+              <template v-else>
+                <q-icon size="sm" name="eva-wifi-off-outline" />
+              </template>
+              <div class="float-right text-bold">${{ space.price }}</div>
             </div>
-            <div class="col-12 text-right text-bold">${{ space.price }}</div>
           </div>
         </div>
       </div>
@@ -35,7 +65,7 @@
 
 <script>
 import spaceTypes from "src/utils/spaceTypes";
-import SpaceTypeSelection from "components/Space/TypeSelectionCard.vue";
+import dayjs from "dayjs";
 
 export default {
   name: "ComponentSpaceMini",
@@ -45,17 +75,19 @@ export default {
       required: true,
     },
   },
-  components: {
-    SpaceTypeSelection,
-  },
   setup(props) {
     const spaceType = spaceTypes.find(
       (type) => type.value === props.space.type
     );
+    const parseTime = (time) => {
+      return dayjs("2012-01-25" + time).format("hh:mm A");
+    };
+
     return {
       spaceType,
       asssetsRoute:
         process.env.SUPABASE_PROJECT_URL + "/storage/v1/object/public/",
+      parseTime,
     };
   },
 };
