aab971a38fb88ab58e11c4f0c7209751688e0233
Albert Hidalgo <alhidalgodev@gmail.com>
2022-08-15 22:45:31 -0400

    feat
    * Created view location component

---
+++ src/components/Map/ViewLocation.vue
@@ -0,0 +1,78 @@
+<template>
+  <div style="border-radius: 4px" id="map"></div>
+</template>
+
+<script>
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+export default {
+  name: "ComponentViewLocation",
+  created() {
+    setTimeout(() => {
+      this.mapBoxInit();
+      this.setServiceLocation(this.coordinates);
+    }, 1000);
+  },
+  props: ["coordinates"],
+  data() {
+    return {
+      map: {},
+      marker: {},
+    };
+  },
+  methods: {
+    mapBoxInit() {
+      mapboxgl.accessToken = process.env.ACCESS_TOKEN;
+      this.map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [-71.1802393, 18.9657173],
+        zoom: 9,
+      });
+      this.addMapControls();
+    },
+    addMapControls() {
+      this.map.addControl(new mapboxgl.NavigationControl());
+      this.map.addControl(
+        new mapboxgl.GeolocateControl({
+          positionOptions: {
+            enableHighAccuracy: true,
+          },
+          trackUserLocation: true,
+        })
+      );
+    },
+    addMarker() {
+      this.marker = new mapboxgl.Marker({
+        draggable: true,
+      })
+        .setLngLat([0, 0])
+        .addTo(this.map);
+    },
+    setServiceLocation(lngLat) {
+      let coordinates;
+      if (lngLat.longitude) {
+        coordinates = [lngLat.longitude, lngLat.latitude];
+      } else {
+        coordinates = lngLat.replace(" ", "").split(",");
+        coordinates = coordinates.reverse();
+      }
+      // eslint-disable-next-line no-unused-vars
+      var marker = new mapboxgl.Marker().setLngLat(coordinates).addTo(this.map);
+
+      this.map.setCenter(coordinates);
+      this.map.zoomTo(13);
+    },
+  },
+};
+</script>
+<style lang="css">
+.mapboxgl-map {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  left: 0;
+}
+</style>
