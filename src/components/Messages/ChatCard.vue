0b8bbd86fb8bb6559992f167fae5cef0cddfcc9b
Albert Hidalgo <alhidalgodev@gmail.com>
2022-08-21 00:17:31 -0400

    feat
    * Created chat component

---
+++ src/components/Messages/ChatCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    @click="$emit('selected')"
+    :class="`chat-card-container row q-col-gutter-md items-center cursor-pointer ${
+      isSelected ? 'selected' : ''
+    }`"
+  >
+    <div class="col-md-4">
+      <q-avatar size="md" color="primary" text-color="white">
+        {{ chat.to_user.substring(0, 1) }}
+      </q-avatar>
+    </div>
+    <div class="col-md-8">Jhon doe</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComponentChat",
+  props: {
+    chat: {
+      type: Object,
+      required: true,
+    },
+    isSelected: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+<style>
+.chat-card-container.selected {
+  font-weight: bold;
+}
+</style>
