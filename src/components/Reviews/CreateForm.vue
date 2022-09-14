58443174221471668fddccdeb434e9f6e5df6692
Albert Hidalgo <alhidalgodev@gmail.com>
2022-09-13 20:43:24 -0400

    feat
    * Created form component for reviews

---
+++ src/components/Reviews/CreateForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <p>{{ $t("messages.information.howDidweDo") }}</p>
+      <div class="text-center">
+        <q-rating
+          v-model="rating"
+          size="3.5em"
+          color="yellow-8"
+          icon="eva-star-outline"
+          icon-selected="eva-star"
+        />
+      </div>
+    </div>
+    <div class="col-12">
+      <p>{{ $t("messages.information.careShareMoreAboutIt") }}</p>
+      <q-input
+        v-model="comment"
+        outlined
+        :label="$t('common.comment')"
+        type="textarea"
+        :counter="true"
+        :maxlength="100"
+      />
+    </div>
+    <div class="col-12 text-center">
+      <q-btn
+        :disabled="rating === 0"
+        @click="submit"
+        push
+        color="primary"
+        text-color="white"
+        :label="$t('action.publishFeedback')"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "ComponentReviewForm",
+  setup() {
+    const rating = ref(0);
+    const comment = ref("");
+
+    return {
+      rating,
+      comment,
+    };
+  },
+};
+</script>
