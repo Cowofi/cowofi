428ca823dbe815a8cb21bd356930fb11f74597a5
Phillip Cabrera <pcabreram1234@gmail.com>
2022-10-05 22:33:55 -0400

    Shared Button added 'Only UI' Scripts and behaviour remaing

---
+++ src/components/buttons/ShareButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-btn
+      color="primary"
+      label="shared"
+      icon="eva-share-outline"
+      class="flex justify-end"
+      :onclick="handleClick"
+    />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ShareButton",
+  methods: {
+    handleClick() {
+      console.log("Click");
+    },
+  },
+});
+</script>
