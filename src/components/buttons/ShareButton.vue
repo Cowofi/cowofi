2e45ab4e8154322df949acc47a805aa45bcfbd3a
Phillip Cabrera <pcabreram1234@gmail.com>
2022-10-06 21:31:09 -0400

    Social Media Buttons Added

--- src/components/buttons/ShareButton.vue
+++ src/components/buttons/ShareButton.vue
@@ -5,19 +5,53 @@
       label="shared"
       icon="eva-share-outline"
       class="flex justify-end"
-      :onclick="handleClick"
+      :onclick="handleShowModal"
     />
+
+    <q-dialog v-model="showModal" persistent>
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Share on</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none flex flex-center" style="gap: 10px">
+          <q-btn
+            icon="eva-facebook"
+            color="blue-14"
+            round
+            @click="handleFacebookShared"
+          />
+          <q-btn icon="eva-twitter" color="blue-5" round />
+        </q-card-section>
+
+        <q-card-actions align="center">
+          <q-btn flat label="Close" color="secondary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import { shareOnFacebook } from "../../utils/socialMedia";
 
 export default defineComponent({
   name: "ShareButton",
+  data() {
+    return {
+      showModal: false,
+    };
+  },
   methods: {
-    handleClick() {
-      console.log("Click");
+    handleShowModal() {
+      this.$data.showModal = true;
+    },
+    closeModal() {
+      this.$data.showModal = false;
+    },
+    handleFacebookShared() {
+      shareOnFacebook();
     },
   },
 });
---
+++ src/utils/socialMedia.js
@@ -0,0 +1,6 @@
+const shareOnFacebook = () => {
+  window.location.href =
+    "https://www.facebook.com/sharer/sharer.php?u=example.org";
+};
+
+export { shareOnFacebook };
